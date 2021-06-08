8917a8e89474217ab00dc6682023b22d1ce3e43c
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-07 21:01:28 -0500

    create database and table

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE if EXISTS employee_tracker;
+
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+);
+
+CREATE TABLE employees(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NUll,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+
+);
