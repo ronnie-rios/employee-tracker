99e00589126fc5625990f6e72ed8413fe32d40ce
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-11 14:05:29 -0500

    add initial prompt and options

--- index.js
+++ index.js
@@ -5,3 +5,49 @@ const cTable = require('console.table');
 
 const port = process.env.PORT || 3001;
 
+function initialPrompts() {
+    inquirer.createPromptModule([
+        {
+            type: 'list',
+            message: 'Please choose a following option',
+            name: 'options',
+            choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add department',
+            'Add role',
+            'Add Employee' ,
+            'Update emplyoee']
+        }
+    ]).then
+}
+
+function viewDepartments(){
+
+};
+
+function viewRoles(){
+
+};
+
+function viewEmployees(){
+
+};
+
+
+function addDepartment(){
+
+};
+
+function addRole(){
+
+};
+
+function addEmployee(){
+
+};
+
+function updateEmployee(){
+
+};
